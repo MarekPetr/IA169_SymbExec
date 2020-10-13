982681ec06ca63fe48c8934c5bac99391de223ce
Petr Marek <petr.marek18@gmail.com>
2020-10-13 18:24:03 +0200

    SymbExec working

--- symbolicexecutor.py
+++ symbolicexecutor.py
@@ -3,6 +3,8 @@
 from language import Instruction, Variable, Cmp
 from interpreter import ExecutionState, Interpreter
 from z3 import *
+from queue import LifoQueue
+
 
 class SymbolicExecutionState(ExecutionState):
     def __init__(self, pc):
@@ -15,10 +17,13 @@ class SymbolicExecutionState(ExecutionState):
         # new.attr = old.attr, that would
         # only create a reference)
 
+        self.pathCond = [True]
+
     def copy(self):
         n = SymbolicExecutionState(self.pc)
         n.variables = self.variables.copy()
-        n.values = self.values.copy()
+        n.values = self.values.copy()        
+        n.pathCond = self.pathCond.copy()
         n.error = self.error
         return n
 
@@ -46,23 +51,74 @@ class SymbolicExecutor(Interpreter):
     def __init__(self, program):
         super().__init__(program)
 
+        self.stack = LifoQueue()
+        self.executed_paths = 0
+        self.errors = 0
+
     def execProgram(self, state):
         state = self.executeInstruction(state)
         if state and state.error:
             raise RuntimeError(f"Execution error: {state.error}")
 
     def executeJump(self, state):
+        
         jump = state.pc
         condval = state.eval(jump.get_condition())
         if condval is None:
             state.error = f"Using unknown value: {jump.get_condition()}"
             return state
 
-        assert isinstance(condval, BoolRef), f"Invalid condition: {condval}"
-        successorblock = jump.get_operand(0 if condval else 1)
-        state.pc = successorblock[0]
-        return state
+        assert (isinstance(condval, BoolRef) or isinstance(condval, list) 
+                or condval in [True, False])\
+            , f"Invalid condition: {exprs}"
+        
+        s = Solver()
+        pc = state.pathCond.copy()
+        pc.append(condval)
 
+        s.add(pc)
+        if s.check() == sat:
+            pc_state = state.copy()
+            successorblock = jump.get_operand(0)
+            pc_state.pathCond.append(condval)
+            pc_state.pc = successorblock[0]
+            self.stack.put(pc_state)
+
+        s.reset()
+
+        not_pc = state.pathCond.copy()
+        not_pc.append(Not(condval))
+
+        s.add(not_pc)
+        if s.check() == sat:
+            not_pc_state = state.copy()
+            successorblock = jump.get_operand(1)
+            not_pc_state.pathCond.append(condval)
+            not_pc_state.pc = successorblock[0]
+            self.stack.put(not_pc_state)
+        
+        return None
+
+    def executeMem(self, state):
+        instruction = state.pc
+        ty = instruction.get_ty()
+        op = instruction.get_operand(0)
+        if ty == Instruction.LOAD:
+            value = state.read(op)
+            if value is None:
+                state.set(instruction, Int(op.get_name()))
+            else:
+                state.set(instruction, value)
+        elif ty == Instruction.STORE:
+            value = state.eval(op)
+            if value is None:
+                state.error = f"Using unknown value: {op}"
+            state.write(instruction.get_operand(1), value)
+        else:
+            raise RuntimeError(f"Invalid memory instruction: {instruction}")
+
+        return state
+    
     def executeAssert(self, state):
         instruction = state.pc
         condval = state.eval(instruction.get_condition())
@@ -70,63 +126,40 @@ class SymbolicExecutor(Interpreter):
             state.error = f"Using unknown value: {jump.get_condition()}"
             return state
 
+        # TODO FORK HERE
         assert isinstance(condval, BoolRef), f"Invalid condition: {condval}"
-        if condval is False:
-            state.error = f"Assertion failed: {instruction}"
-        return state
-
-    def executeCmp(self, state):
-        cmpinst = state.pc
-        predicate = cmpinst.get_predicate()
-        a = state.eval(cmpinst.get_operand(0))
-        if a is None:
-            state.error = f"Using unknown value: {cmpinst.get_operand(0)}"
-            return state
-        b = state.eval(cmpinst.get_operand(1))
-        if b is None:
-            state.error = f"Using unknown value: {cmpinst.get_operand(1)}"
-            return state
-
         s = Solver()
-        expr = False
-        if predicate == Cmp.LT:
-            s.add(a < b)
-        elif predicate == Cmp.LE:
-            s.add(a <= b)
-        elif predicate == Cmp.GT:
-            s.add(a > b)
-        elif predicate == Cmp.GE:
-            s.add(a >= b)
-        elif predicate == Cmp.EQ:
-            s.add(a == b)
-        elif predicate == Cmp.NE:
-            s.add(a != b)
-        else:
-            raise RuntimeError(f"Invalid comparison: {cmpinst}")
-
-        res = s.check() == CheckSatResult(Z3_L_TRUE)
-        print(type(res))
-        state.set(cmpinst, res)
-        if (res):
-            print(s.model())
-
+        s.add(condval)
+        print(s)
+        if s.check() == unsat:
+            print("fail")
+            state.error = f"Assertion failed: {instruction}"
         return state
 
     def run(self):
         entryblock = program.get_entry()
         state = SymbolicExecutionState(entryblock[0])
 
-        cnt = 0
-        while state:
-            if cnt == 1:
-                pass
-            state = self.executeInstruction(state)
-            if state and state.error:
-                raise RuntimeError(f"Execution error: {state.error}")
-            cnt += 1
+        while True:
+            while state:
+                state = self.executeInstruction(state)
+                if state and state.error:
+                    print(state)
+                    self.errors += 1
+                    break
+            
+            if not self.stack.empty():
+                state = self.stack.get()
+                self.executed_paths += 1
+            else:
+                break
+        
+        # Paths count as leaves, if no leaves, paths = 1
+        if self.executed_paths == 0:
+            self.executed_paths = 1
 
-        #print(f"Executed paths: {self.executed_paths}")
-        #print(f"Error paths: {self.errors}")
+        print(f"Executed paths: {self.executed_paths}")
+        print(f"Error paths: {self.errors}")
 
 if __name__ == "__main__":
     from parser import Parser
